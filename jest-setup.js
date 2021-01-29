6f7715fa39b6a2b82b20899c76f2ff8dee262745
Adam Morsi <adam.morsi@yahoo.com>
2021-01-29 13:33:40 +0200

    add redux-mock-store and update jest setup file

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/react/cleanup-after-each';
+import createStore from 'redux-mock-store';
+
+global.Provider = Provider;
+global.createStore = createStore;
