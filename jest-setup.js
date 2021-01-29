0cea70c1841e85ae16fa54b2d00a3d765b31aec2
Adam Morsi <adam.morsi@yahoo.com>
2021-01-29 15:06:08 +0200

    add provider import in jest setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/react/cleanup-after-each';
 import createStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
 global.Provider = Provider;
 global.createStore = createStore;
