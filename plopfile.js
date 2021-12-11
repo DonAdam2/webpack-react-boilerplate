eabacd26c62ef4092df6fb754f81844f61457397
Adam Morsi <adam.morsi@yahoo.com>
2021-12-11 14:22:22 +0200

    add page generator

--- plopfile.js
+++ plopfile.js
@@ -33,4 +33,39 @@ module.exports = (plop) => {
 			},
 		],
 	});
+
+	plop.setGenerator('page', {
+		description: 'Create a page',
+		// User input prompts provided as arguments to the template
+		prompts: [
+			{
+				// Raw text input
+				type: 'input',
+				// Variable name for this input
+				name: 'name',
+				// Prompt to display on command line
+				message: 'What is your page name?',
+			},
+		],
+		actions: [
+			{
+				// Add a new file
+				type: 'add',
+				// Path for the new file
+				path: 'src/js/containers/pages/{{pascalCase name}}/{{pascalCase name}}.js',
+				// Handlebars template used to generate content of new file
+				templateFile: 'generatorTemplates/Component/Component.js.hbs',
+			},
+			{
+				type: 'add',
+				path: 'src/js/containers/pages/{{pascalCase name}}/{{pascalCase name}}.test.js',
+				templateFile: 'generatorTemplates/Component/Component.test.js.hbs',
+			},
+			{
+				type: 'add',
+				path: 'src/js/containers/pages/{{pascalCase name}}/{{pascalCase name}}.scss',
+				templateFile: 'generatorTemplates/Component/Component.scss.hbs',
+			},
+		],
+	});
 };
