748f06c75c2f17021e0340360a5b0b84bd506c16
Adam Morsi <adam.morsi@yahoo.com>
2021-12-11 14:51:58 +0200

    add custom hook generator

--- plopfile.js
+++ plopfile.js
@@ -1,3 +1,12 @@
+const requireField = (fieldName) => {
+	return (value) => {
+		if (String(value).length === 0) {
+			return fieldName + ' is required';
+		}
+		return true;
+	};
+};
+
 module.exports = (plop) => {
 	plop.setGenerator('component', {
 		description: 'Create a component',
@@ -10,6 +19,8 @@ module.exports = (plop) => {
 				name: 'name',
 				// Prompt to display on command line
 				message: 'What is your component name?',
+				// make user that component name is not empty
+				validate: requireField('name'),
 			},
 		],
 		actions: [
@@ -36,24 +47,18 @@ module.exports = (plop) => {
 
 	plop.setGenerator('page', {
 		description: 'Create a page',
-		// User input prompts provided as arguments to the template
 		prompts: [
 			{
-				// Raw text input
 				type: 'input',
-				// Variable name for this input
 				name: 'name',
-				// Prompt to display on command line
 				message: 'What is your page name?',
+				validate: requireField('name'),
 			},
 		],
 		actions: [
 			{
-				// Add a new file
 				type: 'add',
-				// Path for the new file
 				path: 'src/js/containers/pages/{{pascalCase name}}/{{pascalCase name}}.js',
-				// Handlebars template used to generate content of new file
 				templateFile: 'generatorTemplates/Component/Component.js.hbs',
 			},
 			{
@@ -71,24 +76,18 @@ module.exports = (plop) => {
 
 	plop.setGenerator('container', {
 		description: 'Create a container',
-		// User input prompts provided as arguments to the template
 		prompts: [
 			{
-				// Raw text input
 				type: 'input',
-				// Variable name for this input
 				name: 'name',
-				// Prompt to display on command line
 				message: 'What is your container name?',
+				validate: requireField('name'),
 			},
 		],
 		actions: [
 			{
-				// Add a new file
 				type: 'add',
-				// Path for the new file
 				path: 'src/js/containers/{{pascalCase name}}/{{pascalCase name}}.js',
-				// Handlebars template used to generate content of new file
 				templateFile: 'generatorTemplates/Component/Component.js.hbs',
 			},
 			{
@@ -100,6 +99,25 @@ module.exports = (plop) => {
 				type: 'add',
 				path: 'src/js/containers/{{pascalCase name}}/{{pascalCase name}}.scss',
 				templateFile: 'generatorTemplates/Component/Component.scss.hbs',
+			},
+		],
+	});
+
+	plop.setGenerator('hook', {
+		description: 'Create a custom react hook',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What is your hook name?',
+				validate: requireField('name'),
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: 'src/js/customHooks/{{camelCase name}}.js',
+				templateFile: 'generatorTemplates/hook.js.hbs',
 			},
 		],
 	});
