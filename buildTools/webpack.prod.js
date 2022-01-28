0aced9b8db03d1246693692fc2b0502319831b1c
Adam Morsi <adam.morsi@yahoo.com>
2022-01-28 12:06:01 +0200

    update terser plugin options

--- buildTools/webpack.prod.js
+++ buildTools/webpack.prod.js
@@ -22,10 +22,30 @@ module.exports = (env, options) => {
 					parallel: true,
 					// https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
 					terserOptions: {
-						ecma: 5,
-						warnings: false,
+						parse: {
+							// We want terser to parse ecma 8 code. However, we don't want it
+							// to apply any minification steps that turns valid ecma 5 code
+							// into invalid ecma 5 code. This is why the 'compress' and 'output'
+							// sections only apply transformations that are ecma 5 safe
+							ecma: 8,
+						},
 						compress: {
+							ecma: 5,
+							warnings: false,
+							// Disabled because of an issue with Uglify breaking seemingly valid code
+							comparisons: false,
+							// Disabled because of an issue with Terser breaking valid code
+							inline: 2,
 							drop_console: true,
+						},
+						mangle: {
+							safari10: true,
+						},
+						output: {
+							ecma: 5,
+							comments: false,
+							// Turned on because emoji and regex is not minified properly using default
+							ascii_only: true,
 						},
 					},
 				}),
