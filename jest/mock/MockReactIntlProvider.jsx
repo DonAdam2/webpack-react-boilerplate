0adf874a8757974a2ffc34a0de4cfdc019800e2c
Adam Morsi <adam.morsi@yahoo.com>
2022-02-02 19:08:13 +0200

    add mock react intl provider for testing

---
+++ jest/mock/MockReactIntlProvider.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+
+const MockReactIntlProvider = ({ children, locale = 'en' }) => (
+	<IntlProvider locale={locale}>{children}</IntlProvider>
+);
+
+export default MockReactIntlProvider;
