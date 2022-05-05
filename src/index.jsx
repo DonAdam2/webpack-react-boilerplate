0943169379538bc246ebf195a7a64f6e1fcefdf8
DonAdam2 <adam.morsi@yahoo.com>
2022-05-05 12:29:47 +0300

    upgrade react-redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -18,9 +18,11 @@ const store = configureStore(),
   root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
