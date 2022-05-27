6604042c911d287130213e6243f0128ac5e73b92
DonAdam2 <adam.morsi@yahoo.com>
2022-05-27 15:41:26 +0300

    simplify app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,11 @@
-import { render, screen } from '@testing-library/react';
+// all providers mock
+import { render, screen } from '@/jest/mocks/OverrideRenderOfRTL';
 //component
 import App from './App';
-// mock store provider
-import MockReduxProvider from '@/jest/mocks/MockReduxProvider';
 
 describe('App Component', () => {
   test('renders webpack react boilerplate', async () => {
-    render(
-      <MockReduxProvider>
-        <App />
-      </MockReduxProvider>
-    );
+    render(<App />);
     const title = await screen.findByRole('heading', { name: /webpack react boilerplate/i });
     expect(title).toBeInTheDocument();
   });
