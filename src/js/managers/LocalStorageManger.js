fcab31b3a4ef02b182c5867522118a47a9950910
DonAdam2 <adam.morsi@yahoo.com>
2023-08-02 10:02:06 +0300

    update local storage to fix security issue

--- src/js/managers/LocalStorageManger.js
+++ src/js/managers/LocalStorageManger.js
@@ -3,24 +3,62 @@ import { decryptData, encryptData } from '../constants/Helpers';
 
 class LocalStorageManager {
   static setItem(key, data) {
-    const encryptedValue = encryptData(data);
-    localStorage.setItem(key, encryptedValue);
-  }
-  static getItem(key) {
-    const value = localStorage.getItem(key);
-    try {
-      return decryptData(value);
-    } catch (e) {
-      return value;
+    if (this.storageAvailable()) {
+      const encryptedValue = encryptData(data);
+      localStorage.setItem(key, encryptedValue);
     }
   }
+  static getItem(key) {
+    if (this.storageAvailable()) {
+      const value = localStorage.getItem(key);
+      try {
+        return decryptData(value);
+      } catch (e) {
+        return value;
+      }
+    }
+    return undefined;
+  }
   static removeItem(key) {
-    const value = this.getItem(key);
-    localStorage.removeItem(key);
-    return value;
+    if (this.storageAvailable()) {
+      const value = this.getItem(key);
+      localStorage.removeItem(key);
+      return value;
+    }
+    return undefined;
   }
   static clear() {
-    localStorage.clear();
+    if (this.storageAvailable()) {
+      localStorage.clear();
+    }
+  }
+  //used to solve the following issue
+  //SecurityError: The operation is insecure.
+  static storageAvailable() {
+    let storage;
+    try {
+      storage = window.localStorage;
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        // everything except Firefox
+        (e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
   }
 }
 
