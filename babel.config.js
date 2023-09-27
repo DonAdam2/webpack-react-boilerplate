5bb64cedfd2e1ce2546d82a52f8ff5798811cfcc
DonAdam2 <adam.morsi@yahoo.com>
2023-09-27 16:41:12 +0300

    add babel plugin macros

--- babel.config.js
+++ babel.config.js
@@ -30,6 +30,7 @@ module.exports = (api) => {
     plugins = [
       '@babel/plugin-transform-runtime',
       '@babel/plugin-syntax-dynamic-import',
+      'babel-plugin-macros',
       // Applies the react-refresh Babel plugin on non-production modes only
       ...(api.env() === 'development' ? ['react-refresh/babel'] : []),
     ];
