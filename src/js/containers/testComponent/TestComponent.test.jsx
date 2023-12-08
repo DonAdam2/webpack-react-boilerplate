69b0288b63c1bc3b1278e9994bc85c26d3288852
“ <adam.morsi@yahoo.com>
2023-12-08 16:55:14 +0200

    update test component tests

--- src/js/containers/testComponent/TestComponent.test.jsx
+++ src/js/containers/testComponent/TestComponent.test.jsx
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux';
-// all providers mock
-import { screen, fireEvent } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 // snapshots renderer
 import renderer from 'react-test-renderer';
 // mock store provider
@@ -10,8 +9,8 @@ import setupStore from '@/jest/mocks/store';
 //components
 import TestComponent from './TestComponent';
 
-describe('testComponent.jsx', () => {
-  it('snapshot renders correctly, truthy values', () => {
+describe('TestComponent', () => {
+  it('should render snapshot correctly, truthy values', () => {
     const store = setupStore(),
       tree = renderer.create(
         <Provider store={store}>
@@ -20,23 +19,29 @@ describe('testComponent.jsx', () => {
       );
     expect(tree).toMatchSnapshot();
   });
-  it('state should be updated when user clicks the button', () => {
-    // using setupStore function (You don't need to pass the preloaded state)
-    /*const store = setupStore({
-      app: {
-        testString: 'Initial test',
-      },
-    });
-    renderWithRedux(<TestComponent />, { store });*/
-    //using custom render (You don't need to pass the preloaded state)
-    renderWithRedux(<TestComponent />, {
+
+  it('should render environment API', () => {
+    renderWithRedux(<TestComponent />);
+    expect(screen.getByText(/Current environment API is/i)).toBeInTheDocument();
+  });
+
+  it('should render testAction from store', () => {
+    renderWithRedux(<TestComponent />);
+    expect(screen.getByText(/initial test/i)).toBeInTheDocument();
+  });
+
+  it('should update testAction when user clicks the button', () => {
+    //pass preloadedState if you need to pass custom state to the store
+    /*renderWithRedux(<TestComponent />, {
       preloadedState: {
         app: {
           testString: 'Initial test',
         },
       },
-    });
-    fireEvent.click(screen.getByRole('button', { name: /change text/i }));
+    });*/
+
+    renderWithRedux(<TestComponent />);
+    fireEvent.click(screen.getByText(/change text/i));
     expect(screen.getByText(/final test/i)).toBeInTheDocument();
   });
 });
